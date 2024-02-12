ac7eebcb12370b75013669ba9363b15886e26230
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2024-02-12 20:08:01 +0530

    NavUI changes, PokemonList audio refinement and audio addition to the Nav-Buttons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import bgMusicFile from "../assets/Littleroot Town.mp3";
-import buttonSound from "../assets/button-sound.mp3";
+import buttonSound from "../assets/click-sound.mp3";
 import notificationSound from "../assets/notification-sound.mp3";
 import { useEffect } from "react";
 import "./Nav.css";
@@ -8,13 +8,26 @@ const Nav = () => {
   const bgM = new Audio(bgMusicFile);
   const btnSound = new Audio(buttonSound);
   const notiSound = new Audio(notificationSound);
+
   function handlePause() {
     if (bgM.paused) {
       bgM.play();
-    } else {
-      bgM.pause();
+      return;
     }
+    bgM.pause();
   }
+  function handleMouseEnter() {
+    notiSound.play();
+  }
+  function handleMouseLeave() {
+    notiSound.pause();
+    notiSound.currentTime = 0;
+  }
+  function handleBtnClickSound() {
+    btnSound.currentTime = 0;
+    btnSound.play();
+  }
+
   useEffect(() => {
     // bgM.play();
   }, []);
@@ -24,45 +37,37 @@ const Nav = () => {
       <p
         onClick={handlePause}
         className="nav-btn"
-        onMouseEnter={() => notiSound.play()}
-        onMouseLeave={() => {
-          notiSound.pause();
-          notiSound.currentTime = 0;
-        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         Toggle Music
       </p>
       <Link
         to={"/pokemon-list"}
+        onClick={handleBtnClickSound}
         className="nav-btn"
-        onMouseEnter={() => notiSound.play()}
-        onMouseLeave={() => {
-          notiSound.pause();
-          notiSound.currentTime = 0;
-        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         Goto List
       </Link>
       <h1 id="logo">The PokiMen Project</h1>
-      <Link
-        to={"/about"}
+      <p
+        // to={"/about"}
         className="nav-btn"
-        onMouseEnter={() => notiSound.play()}
-        onMouseLeave={() => {
-          notiSound.pause();
-          notiSound.currentTime = 0;
-        }}
+        // onClick={handleBtnClickSound}
+        // onMouseEnter={handleMouseEnter}
+        // onMouseLeave={handleMouseLeave}
+        style={{cursor: "not-allowed"}}
       >
-        toBeImplimented
-      </Link>
+        ToBeImplimented
+      </p>
       <Link
         to={"/about"}
         className="nav-btn"
-        onMouseEnter={() => notiSound.play()}
-        onMouseLeave={() => {
-          notiSound.pause();
-          notiSound.currentTime = 0;
-        }}
+        onClick={handleBtnClickSound}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         AboutMe
       </Link>
